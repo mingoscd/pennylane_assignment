a71f5b21000477bdfa2569067745d1ac82611ee6
domingo <dcividanes91@gmail.com>
2023-09-13 14:11:29 +0900

    modify seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,7 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
 ingredients_path = File.join(Rails.root, 'data', 'import', 'ingredients.json')
-ingredients_file = File.read(ingredients_path)
-ingredients = JSON.parse(ingredients_file)
+ingredients = JSON.parse(File.read(ingredients_path))
 
 # insert all ingredients listed in the ingredients file. Silent if any error happens
 Ingredient.insert_all(ingredients)
@@ -16,23 +15,24 @@ Ingredient.insert_all(ingredients)
 # NOTE: If the recipe ingredient is not in the ingredients table, ingredient_id will be null
 
 recipes_path = File.join(Rails.root, 'data', 'import', 'recipes.json')
-recipes_file = File.read(recipes_path)
-recipes = JSON.parse(recipes_file)
+recipes = JSON.parse(File.read(recipes_path))
 
-recipes.each do |recipe|
-  new_recipe = Recipe.create!(
-    title: recipe['title'],
-    cook_time: recipe['cook_time'],
-    prep_time: recipe['prep_time'],
-    ratings: recipe['ratings'],
-    cuisine: recipe['cuisine'].downcase,
-    category: recipe['category'].downcase,
-    author: recipe['author'],
-    image: recipe['image']
-  )
-  
-  ingredients = ParseIngredients.call(ingredients: recipe['ingredients'])
-                                .map { |h| h.merge(recipe_id: new_recipe.id) }
-  RecipeIngredient.insert_all(ingredients)
+# Split the recipes into batches to reduce memory usage
+recipes.each_slice(100) do |recipe_batch|
+  recipe_batch.each do |recipe|
+    new_recipe = Recipe.create!(
+      title: recipe['title'],
+      cook_time: recipe['cook_time'],
+      prep_time: recipe['prep_time'],
+      ratings: recipe['ratings'],
+      cuisine: recipe['cuisine'].downcase,
+      category: recipe['category'].downcase,
+      author: recipe['author'],
+      image: recipe['image']
+    )
+    
+    ingredients = ParseIngredients.call(ingredients: recipe['ingredients'])
+                                  .map { |h| h.merge(recipe_id: new_recipe.id) }
+    RecipeIngredient.insert_all(ingredients)
+  end
 end
-
