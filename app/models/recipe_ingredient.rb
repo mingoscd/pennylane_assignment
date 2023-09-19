1ac1635f2762adefa75bd5c14f85f8283ab775c2
domingo <dcividanes91@gmail.com>
2023-09-19 17:35:42 +0900

    Add RecipeIngredient#highlighted_description to highlight the checked ingredients

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -3,4 +3,16 @@
 class RecipeIngredient < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient, optional: true
+
+  default_scope { includes(:ingredient) }
+
+  def highlighted_description
+    return description if ingredient_id.nil?
+
+    
+    description.gsub(/#{Regexp.quote(ingredient.name)}/i, "<strong>#{ingredient.name}</strong>")
+               .gsub(' <strong>', "&nbsp<strong>") # fix spaces before and after the tag
+               .gsub('</strong> ', "</strong>&nbsp") 
+               .html_safe
+  end
 end
